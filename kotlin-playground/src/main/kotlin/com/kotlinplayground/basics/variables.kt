68952d8f1dd3ba0cf3bda64932c458e13153f464
zalacop <zala.cop@gmail.com>
2026-08-17 04:34:08 +0200

    introduction to different variables

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/variables.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/variables.kt
@@ -1,6 +1,14 @@
 package org.example.com.kotlinplayground.basics
 
 fun main() {
-    val name = "Zala"
+    val name : String = "Zala"
     println(name)
+
+    //name = "Zala1"
+
+    var age : Int = 34
+    println(age)
+    age = 35
+    println(age)
+
 }
