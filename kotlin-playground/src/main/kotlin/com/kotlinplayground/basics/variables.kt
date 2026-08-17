65194710b0ee376994ca6aadbd9e23abf73ffd2f
zalacop <zala.cop@gmail.com>
2026-08-17 04:55:33 +0200

    learning strings and its related operations

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/variables.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/variables.kt
@@ -1,14 +1,29 @@
 package org.example.com.kotlinplayground.basics
 
 fun main() {
-    val name : String = "Zala"
+    val name = "Zala"
     println(name)
 
     //name = "Zala1"
 
-    var age : Int = 34
+    var age = 34
     println(age)
     age = 35
     println(age)
+
+    val salary = 30000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    println("course = $course and the course length is ${course.length}")       //String Interpolation
+
+    val multiLine = "ABC \n DEF"
+    println(multiLine)
+
+    val multiLine1 = """
+        ABC
+        DEF
+    """.trimIndent()           //Multiline strings using TripleQuotes
+    println(multiLine1)
 
 }
