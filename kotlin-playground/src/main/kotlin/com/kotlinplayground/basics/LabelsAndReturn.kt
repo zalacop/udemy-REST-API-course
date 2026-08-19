ec79375a4b2db7828a8450a5828b4d0f1a3608e3
zalacop <zala.cop@gmail.com>
2026-08-20 01:36:05 +0200

    adds continue loop

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/LabelsAndReturn.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/LabelsAndReturn.kt
@@ -23,7 +23,8 @@ fun label() {
         //if(i==3) break@loop
         innerloop@ for (j in 1..5) {
             println("j in label : $j")
-            if(j==2) break@innerloop
+            //if(j==2) break@innerloop
+            if(j==2) continue@loop
         }
     }
 }
