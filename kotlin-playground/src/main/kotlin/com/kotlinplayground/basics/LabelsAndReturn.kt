b4fe18a123bc079c731478dbfe2bd0da2578e700
zalacop <zala.cop@gmail.com>
2026-08-20 01:30:25 +0200

    learning labels and inner loops

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/LabelsAndReturn.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/LabelsAndReturn.kt
@@ -7,6 +7,18 @@ fun main() {
         if(i==3) break
     }
 
-    println("End of the Program!")
+    label()
 
+    println("End of the Program!")
+}
+
+fun label() {
+    loop@ for (i in 1..5) {
+        println("i in label : $i")
+        //if(i==3) break@loop
+        innerloop@ for (j in 1..5) {
+            println("j in label : $j")
+            if(j==2) break@innerloop
+        }
+    }
 }
