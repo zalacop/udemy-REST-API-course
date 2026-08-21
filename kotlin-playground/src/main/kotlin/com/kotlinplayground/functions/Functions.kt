95b9646a4459373ae409d9f8981b99e6eb2dfffa
zalacop <zala.cop@gmail.com>
2026-08-21 04:10:59 +0200

    start learning about functions

---
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
@@ -0,0 +1,17 @@
+package org.example.com.kotlinplayground.functions
+
+fun printName(name: String) {
+    println("Name is: $name")
+}
+
+fun addition(i: Int, i2: Int): Int {
+    TODO("Not yet implemented")
+}
+
+fun main() {
+    printName("Zala")
+    val result = addition(1,2)
+}
+
+
+
