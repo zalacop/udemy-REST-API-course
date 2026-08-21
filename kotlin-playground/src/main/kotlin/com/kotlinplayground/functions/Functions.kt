937ba9641b6ef9f03071befb9b76db8ae249c81d
zalacop <zala.cop@gmail.com>
2026-08-21 04:21:04 +0200

    learning about parameters in functions

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
@@ -1,16 +1,25 @@
 package org.example.com.kotlinplayground.functions
 
-fun printName(name: String) {
+fun printName(name: String) : Unit {    //Unit is a type in kotlin which represents no value
     println("Name is: $name")
 }
 
-fun addition(i: Int, i2: Int): Int {
-    TODO("Not yet implemented")
+fun addition(x: Int, y: Int): Int {
+   return x + y
 }
 
+fun addition_approach1(x: Int, y: Int) = x + y
+
 fun main() {
-    printName("Zala")
+
+    val unit = printName("Zala")    //Unit is a type in kotlin which represents no value
+    println("Unit is $unit")
     val result = addition(1,2)
+    println("Result is $result")
+
+    val result1 = addition_approach1(1,2)
+    println("Result is $result1")
+
 }
 
 
