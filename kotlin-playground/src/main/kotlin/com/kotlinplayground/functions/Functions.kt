8585bb0e7ca509f75fe38a3436922b3e8238011f
zalacop <zala.cop@gmail.com>
2026-08-21 04:35:06 +0200

    learning about default parameters and named arguments/parameters

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
@@ -1,6 +1,8 @@
 package org.example.com.kotlinplayground.functions
 
-fun printName(name: String) : Unit {    //Unit is a type in kotlin which represents no value
+import java.time.LocalDate
+
+fun printName(name: String) : Unit {    //Unit is a type in kotlin which represents no value.
     println("Name is: $name")
 }
 
@@ -10,16 +12,26 @@ fun addition(x: Int, y: Int): Int {
 
 fun addition_approach1(x: Int, y: Int) = x + y
 
+fun printPersonDetails(name: String,
+                       email: String = "",
+                       dob: LocalDate = LocalDate.now(),) {
+    println("Name is $name and the email is $email and the dob is $dob")
+
+}
+
 fun main() {
 
-    val unit = printName("Zala")    //Unit is a type in kotlin which represents no value
+   /* val unit = printName("Zala")    //Unit is a type in kotlin which represents no value
     println("Unit is $unit")
     val result = addition(1,2)
     println("Result is $result")
 
     val result1 = addition_approach1(1,2)
-    println("Result is $result1")
+    println("Result is $result1")*/
 
+    printPersonDetails(name = "Zala", email = "abs@gmail.com", dob = LocalDate.parse("1990-05-29"))
+
+    printPersonDetails(name = "Zala")
 }
 
 
