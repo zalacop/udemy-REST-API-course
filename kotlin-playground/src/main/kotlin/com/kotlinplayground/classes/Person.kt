6a066cca6096fd2d9bdfe2fd35aefe5cc270f1d2
zalacop <zala.cop@gmail.com>
2026-08-24 04:46:49 +0200

    adds default values

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -1,7 +1,7 @@
 package org.example.com.kotlinplayground.classes
 
-class Person(val name: String,       // Object oriented programming is fundamentally the blueprint for creating objects
-             val age: Int) {
+class Person(val name: String = "",       // Object oriented programming is fundamentally the blueprint for creating objects
+             val age: Int = 0) {
 
     fun action() {
         println("Person walks.")
@@ -12,4 +12,7 @@ fun main() {
     val person = Person(name = "Alex", age = 25)
     person.action()
     println("Name is : ${person.name} and age is ${person.age}")
+
+    val person1 = Person()
+    println("Name is : ${person1.name} and age is ${person1.age}")
 }
