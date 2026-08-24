8d992cc56c3d1247252cb822ba80e74eddc9f79d
zalacop <zala.cop@gmail.com>
2026-08-24 04:32:45 +0200

    learning about classes

---
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -0,0 +1,13 @@
+package org.example.com.kotlinplayground.classes
+
+class Person {
+
+    fun action() {
+        println("Person walks.")
+    }
+}
+
+fun main() {
+    val person = Person()
+    person.action()
+}
