65233df5478c47c0ed90ca1a832f7a5a1778aa2f
zalacop <zala.cop@gmail.com>
2026-08-26 23:16:49 +0200

    adds practical example

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -1,7 +1,13 @@
 package org.example.com.kotlinplayground.classes
 
-class Person(val name: String = "",       // Object oriented programming is fundamentally the blueprint for creating objects
-             val age: Int = 0) {
+class Person(
+    val name: String = "",       // Object oriented programming is fundamentally the blueprint for creating objects
+    val age: Int = 0
+) {
+    var email: String = ""
+    constructor(_email: String) : this() {
+        email = _email
+    }
 
     fun action() {
         println("Person walks.")
@@ -15,4 +21,7 @@ fun main() {
 
     val person1 = Person()
     println("Name is : ${person1.name} and age is ${person1.age}")
+
+    val person2 = Person(_email = "abc@gmail.com")
+    println("Name is : ${person2.name}, age is ${person2.age} and the email is ${person2.email}")
 }
