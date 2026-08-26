d8ad3eba683f2a96a60fa2a9250d04d4e244ef2d
zalacop <zala.cop@gmail.com>
2026-08-26 22:48:04 +0200

    making it work

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Item.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Item.kt
@@ -1,11 +1,16 @@
 package org.example.com.kotlinplayground.classes
 
-class Item {
+class Item() {
 
     var name: String = ""
 
     constructor(_name: String) : this() {
         name = _name
     }
+}
 
+
+fun main() {
+    val item = Item(_name = "iPhone")
+    println("Name is : ${item.name}")
 }
