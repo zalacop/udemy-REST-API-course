d67e5fcd2785e8beafae082f8f1acec95be3f17c
zalacop <zala.cop@gmail.com>
2026-08-27 06:07:41 +0200

    learning .copy

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Course.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Course.kt
@@ -11,7 +11,11 @@ fun main() {
 
     //println(course)
 
-    val course1 = Course(id = 1, name = "Reactive Programming in modern Java using Project reactor", author = "Alice")
+    val course1 = Course(id = 2, name = "Reactive Programming in modern Java using Project reactor", author = "Alice")
 
     println("Checking object equality : ${course == course1}")
+
+    val course2 = course1.copy(id = 3, author = "Dilip" )
+
+    println(course2)
 }
