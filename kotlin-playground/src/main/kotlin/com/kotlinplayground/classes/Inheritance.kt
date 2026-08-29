65fcd5fc618a6b5734c73badbecba8fb5e37f6f2
zalacop <zala.cop@gmail.com>
2026-08-29 04:17:48 +0200

    adds 2 classes

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
@@ -1,5 +1,17 @@
 package org.example.com.kotlinplayground.classes
 
-open class User (val name: String) {
+open class User(val name: String) {
 
+}
+
+class Student(name: String) : User(name)
+
+class Instructor(name: String) : User(name)
+
+fun main() {
+    val student = Student("Zala")
+    println("Name is: ${student.name}")
+
+    val instructor = Instructor("Dilip")
+    println("Name is: ${instructor.name}")
 }
