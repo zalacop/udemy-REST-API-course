9544035dbceec205b9cd0beff2727a0400267aff
zalacop <zala.cop@gmail.com>
2026-08-30 02:34:50 +0200

    learning about overriding variables

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
@@ -2,14 +2,18 @@ package org.example.com.kotlinplayground.classes
 
 open class User(val name: String) {
 
+    open var isLoggedIn: Boolean = false
     open fun login() {
         println("Inside user login")
     }
 }
 
 class Student(name: String) : User(name) {
+
+    override var isLoggedIn: Boolean = false
     override fun login() {
         println("Inside student login")
+        super.login()
     }
 }
 
@@ -19,6 +23,8 @@ fun main() {
     val student = Student("Zala")
     println("Name is: ${student.name}")
     student.login()
+    student.isLoggedIn = true
+    println("Logged in value is: ${student.isLoggedIn}")
 
     val instructor = Instructor("Dilip")
     println("Name is: ${instructor.name}")
