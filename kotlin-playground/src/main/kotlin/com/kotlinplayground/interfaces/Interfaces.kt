ae991adeada1b90170c71e5080b46154964469fb
zalacop <zala.cop@gmail.com>
2026-09-01 04:35:51 +0200

    learning about interfaces

---
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/interfaces/Interfaces.kt
@@ -0,0 +1,23 @@
+package org.example.com.kotlinplayground.interfaces
+
+import org.example.com.kotlinplayground.classes.Course
+
+interface CourseRepository {
+
+    fun getById(id: Int): Course
+
+}
+
+class SqlCourseRepository : CourseRepository {
+    override fun getById(id: Int): Course {
+        return Course(id,
+            name = "Reactive Programming in modern Java using Project reactor",
+            author = "Alice")
+    }
+}
+
+fun main() {
+    val sqlCourseRepository = SqlCourseRepository()
+    val course = sqlCourseRepository.getById(id = 1)
+    println("Course is $course")
+}
