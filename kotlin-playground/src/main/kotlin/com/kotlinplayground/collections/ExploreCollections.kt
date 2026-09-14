32d29877d6baf0ff4b4315c73d427903e1ca52ea
zalacop <zala.cop@gmail.com>
2026-09-14 23:39:41 +0200

    learning about mutable collections

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreCollections.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreCollections.kt
@@ -3,5 +3,10 @@ package org.example.com.kotlinplayground.collections
 fun main() {
     val names = listOf("Alex", "Ben", "Chloe")
     println("Names: $names")
-    
+
+
+    val namesMutableList = mutableListOf("Alex", "Ben", "Chloe")
+    println("Names mutable list before add: $namesMutableList")
+    namesMutableList.add("Adam")
+    println("Names mutable list after add: $namesMutableList")
 }
