35094f6c1de88d28d8579f95e88575c6e88b7ba5
zalacop <zala.cop@gmail.com>
2026-09-18 00:06:16 +0200

    adds lambda explanation

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
@@ -17,3 +17,7 @@ fun main() {
     println("Multiply Lambda : $multiplyResult")
 
 }
+
+// Lambda expressions are small pieces of code that can be passed to other functions
+// Benefit of Lambda: - you can assign the behavior to a variable
+//                    - the advantage is that, you can pass the lambda as an argument to other functions
