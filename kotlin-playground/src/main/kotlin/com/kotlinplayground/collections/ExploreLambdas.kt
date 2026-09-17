3437323c9d75cefe25bfd6555b4c77ecc8112426
zalacop <zala.cop@gmail.com>
2026-09-17 23:58:22 +0200

    learning about lambdas

---
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
@@ -0,0 +1,11 @@
+package org.example.com.kotlinplayground.collections
+
+fun main() {
+
+
+    val addLambda = { x: Int -> x + x }
+
+     val addResult = addLambda(3)
+    println("Add Lambda : $addResult")
+
+}
