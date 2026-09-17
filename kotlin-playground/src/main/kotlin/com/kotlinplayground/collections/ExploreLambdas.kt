d5a9924f2055f03929ec0cfe4b952c56e10f26eb
zalacop <zala.cop@gmail.com>
2026-09-18 00:03:03 +0200

    adds a multiline lambda

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
@@ -8,7 +8,10 @@ fun main() {
      val addResult = addLambda(3)
     println("Add Lambda : $addResult")
 
-    val multiplyLambda = { x: Int, y: Int -> x * y }
+    val multiplyLambda = { x: Int, y: Int ->
+        println("Multiply x is $x and y is $y")
+        x * y
+    }
 
     val multiplyResult = multiplyLambda(2, 3)
     println("Multiply Lambda : $multiplyResult")
