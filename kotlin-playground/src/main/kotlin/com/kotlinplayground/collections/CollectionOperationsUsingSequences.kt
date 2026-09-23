84d477037cd72a319af6cb44f6c9e25b6675a1d7
zalacop <zala.cop@gmail.com>
2026-09-24 00:47:24 +0200

    showing when sequences are beneficial

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/CollectionOperationsUsingSequences.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/CollectionOperationsUsingSequences.kt
@@ -16,6 +16,16 @@ fun main() {
     val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
 
     exploreFilterUsingSequence(courseList(), devPredicate)
+
+    val range = 1 .. 1000000000
+
+    range
+        .asSequence()
+        .map { it.toDouble() }
+        .take(40)
+        .forEach {
+            println("Value is : $it")
+        }
 }
 
 fun exploreFilterUsingSequence(courseList: MutableList<Course>,
