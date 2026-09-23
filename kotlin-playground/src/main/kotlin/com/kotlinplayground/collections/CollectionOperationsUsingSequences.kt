63d2d65b3f7cc4859393ec15b4081ebf786a9075
zalacop <zala.cop@gmail.com>
2026-09-24 00:40:57 +0200

    implementing a real use case

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/CollectionOperationsUsingSequences.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/CollectionOperationsUsingSequences.kt
@@ -1,7 +1,11 @@
 package org.example.com.kotlinplayground.collections
 
+import org.example.com.kotlinplayground.dataset.Course
+import org.example.com.kotlinplayground.dataset.CourseCategory
+import org.example.com.kotlinplayground.dataset.courseList
+
 fun main() {
-    
+
     val namesListUsingSequence = listOf("alice", "bob", "charlie")
         .asSequence()
         .filter { it.length >= 4 }
@@ -9,4 +13,20 @@ fun main() {
         .toList()
 
     println("Names list using sequences : $namesListUsingSequence")
+    val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
+
+    exploreFilterUsingSequence(courseList(), devPredicate)
+}
+
+fun exploreFilterUsingSequence(courseList: MutableList<Course>,
+                  predicate: (Course) -> Boolean) {
+
+    val developmentCourses = courseList
+        /*.filter { it.category == CourseCategory.DEVELOPEMENT }*/
+        .asSequence()
+        .filter { predicate.invoke(it) }
+        .forEach {
+            println("devCourses : $it")
+        }
+
 }
