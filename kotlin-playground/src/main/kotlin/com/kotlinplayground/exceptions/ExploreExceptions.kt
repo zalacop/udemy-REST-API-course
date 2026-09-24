9777366d3a38a9bedb8dec077a09a3f08e1e1cdc
zalacop <zala.cop@gmail.com>
2026-09-25 00:05:23 +0200

    handeling exceptions with try catch

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/exceptions/ExploreExceptions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/exceptions/ExploreExceptions.kt
@@ -3,8 +3,14 @@ package org.example.com.kotlinplayground.exceptions
 fun main() {
 
     println("Name length is :${nameLength("Zala")}")
+    println("Name length is :${nameLength(null)}")
 }
 
-fun nameLength(name: String) : Int {
-    return name!!.length
+fun nameLength(name: String?) : Int? {
+    try {
+        return name!!.length
+    } catch (ex: Exception) {
+        println("Exception is : $ex")
+        return null
+    }
 }
