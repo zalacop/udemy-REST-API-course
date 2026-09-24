b9d39960abbd9ee1552ec5f115b9365e431e07e8
zalacop <zala.cop@gmail.com>
2026-09-25 00:09:27 +0200

    adds nothing type

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/exceptions/ExploreExceptions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/exceptions/ExploreExceptions.kt
@@ -1,16 +1,21 @@
 package org.example.com.kotlinplayground.exceptions
 
+fun returnNothing(): Nothing {
+    throw RuntimeException("This is an exception")
+}
+
 fun main() {
 
     println("Name length is :${nameLength("Zala")}")
     println("Name length is :${nameLength(null)}")
+    returnNothing()
 }
 
 fun nameLength(name: String?) : Int? {
-    try {
-        return name!!.length
+    return try {
+        name!!.length
     } catch (ex: Exception) {
         println("Exception is : $ex")
-        return null
+        null
     }
 }
